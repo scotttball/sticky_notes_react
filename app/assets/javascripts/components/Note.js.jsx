4def0dee132f9fa455649243cf2b20334ddc1e0b
Scott Ball <scotttball@gmail.com>
2016-01-04 14:58:40 -0700

    added sticky notes and full functionality to create

--- app/assets/javascripts/components/Note.js.jsx
+++ app/assets/javascripts/components/Note.js.jsx
@@ -9,12 +9,12 @@ var Note = React.createClass({
   },
 
   showAddForm: function() {
-    this.setState({showAdd: !this.state.show/Add});
+    this.setState({showAdd: !this.state.showAdd});
   },
 
 
-  addNoteName: function(e) {
-    this.setState({noteName: e.currentTarget.value});
+  addNoteTitle: function(e) {
+    this.setState({noteTitle: e.currentTarget.value});
   },
 
   submitNote: function(e) {
@@ -23,29 +23,39 @@ var Note = React.createClass({
     $.ajax({
       url: '/notes',
       type: 'POST',
-      data: {note: {name: this.state.noteName}},
+      data: {note: {title: this.state.noteTitle}},
       success: function(data) {
         var notes = self.state.notes;
-        notes.push({name: data.name, completed: data.complete});
-        self.setState({notes: notes, showAdd: false, noteName: null});
+        notes.push({title: data.title, description: data.description});
+        self.setState({notes: notes, showAdd: false, noteTitle: null});
       }
     });
+  },
+
+  addNoteForm: function() {
+  if(this.state.showAdd) {
+    return(<div>
+            <form onSubmit={this.submitNote}>
+              <div className='input-field'>
+                <input autoFocus='true' placeholder='Note Title' type='text' onChange={this.addNoteTitle} />
+                <button className='btn waves-effects' type='submit'>Submit</button>
+              </div>
+            </form>
+           </div>);
+    }
   },
 
   displayNotes: function() {
     var notes = [];
     for(var i = 0; i < this.state.notes.length; i++){
-      notes.push(<li>
-                  <div className='row'>
-                    <div className='col s10'>
-                     {this.state.notes[i].name}
-                    </div>  
-                    <div className='col s2'>
-                      <input type='checkbox' checed={this.state.notes[i].complete} />
-                      <label>Complete?</label>
+      notes.push(<div className='col m3'>
+                    <div className='card yellow darken-2'>
+                      <div className='card-content white-text center'>
+                       {this.state.notes[i].title}
+                      </div>
                     </div>
                   </div>
-                  </li>);
+                    );
     }
     return notes;
   },
@@ -56,9 +66,12 @@ var Note = React.createClass({
              {this.addNoteForm()}
              <div className='card blue-grey darken-1'>
               <div className='card-content white-text'>
-                <span className='card-title'>To Do</span>
-                {this.displayNotes()}
+                <span className='card-title'>Sticky Notes</span>
+                
               </div>
+             </div>
+             <div className='row'>
+             {this.displayNotes()}
              </div>
            </div>);
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'notes#index'
+  resources :notes
 
 end
